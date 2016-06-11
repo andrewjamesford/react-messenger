d46319b51f1621411beef7f1e528e2759a3f623f
Andrew Ford <andrewfor@datacom.co.nz>
2016-06-11 18:00:46 +1200

    Added the speech api to record and turn audio to text.

--- app/Messenger.tsx
+++ app/Messenger.tsx
@@ -1,17 +1,21 @@
 import * as React from "react";
-import * as Microsoft from "oxford-speech";
+declare var webkitSpeechRecognition: any;
 
 import Message from "./Message";
 import Bubble from "./Bubble";
 
 interface MessengerState {
     inputText: string;
+    isRecording: boolean;
     messagesArray: Array<any>;
 }
 
 let speechKey = "4ea4b0b8bc5447b8bc31e8241ea70b2e";
 
+let recognition = new webkitSpeechRecognition();
+
 export default class Messenger extends React.Component<any, any> {
+
     constructor(props) {
         super(props);
         this.state = {
@@ -21,48 +25,68 @@ export default class Messenger extends React.Component<any, any> {
 
     setText(text) {
         this.setState({
-            inputText: text
+            inputText: text,
+            isRecording: false
         });
-    }
-
-    startTranslate() {
-        console.log('start translate');
-        let client;
-        client = Microsoft.ProjectOxford.SpeechRecognition.SpeechRecognitionServiceFactory.createMicrophoneClient(
-                    Microsoft.ProjectOxford.SpeechRecognition.SpeechRecognitionMode.longDictation,
-                    "en-us",
-                    speechKey,
-                    speechKey);
-
-        client.startMicAndRecognition();
-            setTimeout(function () {
-                client.endMicAndRecognition();
-            }, 5000);
-
-        client.onPartialResponseReceived = function (response) {
-            this.setText(response);
-        };
-
-        client.onFinalResponseReceived = function (response) {
-            this.setText(JSON.stringify(response));
-        };
-
-        client.onIntentReceived = function (response) {
-            this.setText(response);
-        };
     }
 
     sendMessage(text: string) {
         console.log("send message");
     }
 
+    stopRecording() {
+         recognition.stop();
+        this.state.isRecording = false;
+    }
+
+    startTranslate() {
+        if (this.state.isRecording) {
+           this.stopRecording();
+        }
+        else {
+            this.state.isRecording = true;
+
+            recognition.continuous = true;
+            recognition.interimResults = true;
+            recognition.lang = "en-US";
+            recognition.onstart = function() {
+                console.log('recording');
+            };
+
+            recognition.onend = function() {
+                console.log('stopped');
+            };
+
+            recognition.onresult = (event) => {
+
+                let finalText = '';
+                for (let i = event.resultIndex; i < event.results.length; ++i) {
+                    //console.log(event.results[i][0].transcript);
+                    if (event.results[i].isFinal) {
+                        finalText += event.results[i][0].transcript;
+                    } 
+
+                }
+                //console.log(finalText);
+                if (finalText !== '') {
+                    this.setState({
+                        inputText: finalText,
+                        isRecording: this.state.isRecording
+                    });
+                    this.stopRecording();
+                }
+            }
+            recognition.start();
+        };
+    }
+
     renderTextInput() {
 
         return (
             <div className="textInput__cont">
-                <button className="textInput__audio" style={{background: "url(img/mic.svg)"}} onClick={this.startTranslate.bind(this)} />
+                <button className="textInput__audio" style={{ background: "url(img/mic.svg)" }} onClick={this.startTranslate.bind(this) } />
                 <input id="output" className="textInput__input" type="text" placeholder="Type a message..." value={this.state.inputText} />
-                <button className="textInput__btn" onClick={this.sendMessage.bind(this)}>Send</button>
+                <button className="textInput__btn" onClick={this.sendMessage.bind(this) }>Send</button>
             </div>
         );
     }
